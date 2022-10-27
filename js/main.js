4af723f9906746790845a3d0602befc07ea67658
saracassandra7 <saracassandra7@gmail.com>
2022-10-27 17:18:07 +0200

    [add] posts

--- js/main.js
+++ js/main.js
@@ -55,3 +55,48 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+const container = document.getElementById('container');
+
+//funzione che crea i post
+createPosts();
+
+function createPosts(){
+    let output = '';
+    posts.forEach(post => {
+        output += `<div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${post.author.image}" alt="${post.author.name}">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${post.author.name}</div>
+                    <div class="post-meta__time">${post.created}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+        <div class="post__image">
+            <img src="${post.media}" alt="${post.image}">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="${post.id}">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>`
+    })
+
+    container.innerHTML = output;
+}
+
+
